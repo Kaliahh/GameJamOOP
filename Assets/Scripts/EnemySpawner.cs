dfd3d431210ea236921bb7150f077626e6379da0
Rasmus Tollund <44167873+Ragusaen@users.noreply.github.com>
2019-04-25 21:12:38 +0200

    Documentation

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    // Editor fields
     [SerializeField] private int maxEnemies;
     [SerializeField] private float spawnDelay;
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private GameObject spawnPointsParent;
-
+    
+    // Timer that keeps track of time until next possible spawn
     private float spawnTimer = 0.0f;
+    // List of the available spawnpoints
     private List<Transform> spawnPoints = new List<Transform>();
+    // List of spawned enemies
     private List<GameObject> enemies = new List<GameObject>();
 
     private void Start()
@@ -25,17 +29,26 @@ public class EnemySpawner : MonoBehaviour
 
     void Update()
     {
+        // Check if there is room for more enemies
         if (enemies.Count < maxEnemies)
         {
+            // Check if there is still time left until possible spawn
             if (spawnTimer > 0.0f)
             {
+                // Remove passed time from timer, or set to 0 if it is done
                 spawnTimer = (spawnTimer <= Time.deltaTime) ? 0f : spawnTimer - Time.deltaTime;
             } else {
+                // Generate a random index of the spawn points
                 int randomIndex = (int)Random.Range(0, spawnPoints.Count - 0.01f);
+                // Set the spawn position to the random points position
                 Vector3 spawnPos = spawnPoints[randomIndex].position;
+                // Instantiate a new enemy
                 GameObject newEnemy = Instantiate(enemyPrefab, spawnPos, Quaternion.identity, null);
+                // Set the enemies player target to the player
                 newEnemy.GetComponent<EnemyMovement>().Player = player;
+                // Add it to the list of enemies
                 enemies.Add(newEnemy);
+                // Set the spawn timer to the spawn delay
                 spawnTimer = spawnDelay;
             }
         }
