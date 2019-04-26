769a846d8f4985b9d7b0a759a309056b2e901824
Ragusaen <rtollu18@student.aau.dk>
2019-04-25 17:40:21 -0700

    Added EnemySpawner and fixed some stray files

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private int maxEnemies;
+    [SerializeField] private float spawnDelay;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject spawnPointsParent;
+
+    private float spawnTimer = 0.0f;
+    private List<Transform> spawnPoints = new List<Transform>();
+    private List<GameObject> enemies = new List<GameObject>();
+
+    private void Start()
+    {
+        // Initialize list of spawnpoints
+        for (int i = 0; i < spawnPointsParent.transform.childCount; ++i)
+        {
+            spawnPoints.Add(spawnPointsParent.transform.GetChild(i).transform);
+        }
+    }
+
+    void Update()
+    {
+        if (enemies.Count < maxEnemies)
+        {
+            if (spawnTimer > 0.0f)
+            {
+                spawnTimer = (spawnTimer <= Time.deltaTime) ? 0f : spawnTimer - Time.deltaTime;
+            } else {
+                int randomIndex = (int)Random.Range(0, spawnPoints.Count - 0.01f);
+                Vector3 spawnPos = spawnPoints[randomIndex].position;
+                GameObject newEnemy = Instantiate(enemyPrefab, spawnPos, Quaternion.identity, null);
+                newEnemy.GetComponent<EnemyMovement>().Player = player;
+                enemies.Add(newEnemy);
+                spawnTimer = spawnDelay;
+            }
+        }
+    }
+}
